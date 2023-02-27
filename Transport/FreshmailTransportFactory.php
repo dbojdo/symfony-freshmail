d98f9c40cd094049ac1249d51f0ec6b4c7722ccc
Daniel Bojdo <daniel+github@bojdo.eu>
2023-02-27 12:04:27 +0000

    Revert "transport freshmail request"

--- Transport/FreshmailTransportFactory.php
+++ Transport/FreshmailTransportFactory.php
@@ -2,7 +2,6 @@
 
 namespace Symfony\Component\Mailer\Bridge\Freshmail\Transport;
 
-use Symfony\Component\Mailer\Exception\UnsupportedSchemeException;
 use Symfony\Component\Mailer\Transport\AbstractTransportFactory;
 use Symfony\Component\Mailer\Transport\Dsn;
 use Symfony\Component\Mailer\Transport\TransportInterface;
@@ -12,18 +11,11 @@ final class FreshmailTransportFactory extends AbstractTransportFactory
 
     protected function getSupportedSchemes(): array
     {
-        return ['freshmail+api'];
+
     }
 
     public function create(Dsn $dsn): TransportInterface
     {
-        $scheme = $dsn->getScheme();
-        $user = $this->getUser($dsn);
 
-        if ('freshmail+api' === $scheme) {
-            return (new FreshmailApiTransport($user, $this->client, $this->dispatcher, $this->logger));
-        }
-
-        throw new UnsupportedSchemeException($dsn, 'freshmail', $this->getSupportedSchemes());
     }
 }
